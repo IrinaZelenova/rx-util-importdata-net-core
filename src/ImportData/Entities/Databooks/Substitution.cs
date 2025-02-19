a512aacc53732b2630d441eb7503030d7df6b0fc
utkin_pa <utkin_pa@directum.ru>
2025-02-19 15:45:30 +0400

    Обновил версию. Актуализировал шаблоны. Актуализировал код для импорта Замещений.

--- src/ImportData/Entities/Databooks/Substitution.cs
+++ src/ImportData/Entities/Databooks/Substitution.cs
@@ -14,10 +14,10 @@ namespace ImportData.Entities.Databooks
     {
       ResultValues[Constants.KeyAttributes.Status] = Constants.AttributeValue[Constants.KeyAttributes.Status];
 
-      if (ResultValues[Constants.KeyAttributes.DateOfBirth] != null && (DateTimeOffset)ResultValues[Constants.KeyAttributes.StartDate] == DateTimeOffset.MinValue)
+      if (ResultValues[Constants.KeyAttributes.StartDate] != null && (DateTimeOffset)ResultValues[Constants.KeyAttributes.StartDate] == DateTimeOffset.MinValue)
         ResultValues[Constants.KeyAttributes.StartDate] = null;
 
-      if (ResultValues[Constants.KeyAttributes.DateOfBirth] != null && (DateTimeOffset)ResultValues[Constants.KeyAttributes.EndDate] == DateTimeOffset.MinValue)
+      if (ResultValues[Constants.KeyAttributes.StartDate] != null && (DateTimeOffset)ResultValues[Constants.KeyAttributes.EndDate] == DateTimeOffset.MinValue)
         ResultValues[Constants.KeyAttributes.EndDate] = null;
 
       return false;
